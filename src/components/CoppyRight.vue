76b6ea9d2302ec3e0208af4d65c68bdcf374e9c5
tuan nguyen <tuankaka109@gmail.com>
2025-06-19 17:23:14 +0700

    add initial project files, including assets, configuration, and main application setup

---
+++ src/components/CoppyRight.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="copyright">This game owned by RHP Team in Vue 3 course for begginers - view here</p>
+</template>
+
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+</style>
